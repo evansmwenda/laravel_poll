9b9c29ae1a3f71c93ef06785e1a35fd2cdd28e00
evansmwenda <evansmwenda.em@gmail.com>
2024-04-17 14:34:17 +0300

    restrict dashboard to signed in users

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,13 +3,32 @@ import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import Surveys from '../views/Surveys.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
+import store from "../store";
 
 const routes = [
     {
         path: '/',
+        redirect: '/dashboard',
         name: 'Dashboard',
-        component: Dashboard
+        meta: {requiresAuth: true,},
+        component: DefaultLayout,
+        children: [
+           { 
+            path: '/dashboard',
+            name: 'Dashboard',
+            component: Dashboard,
+            },
+            {
+                path: '/surveys',
+                name: 'Surveys',
+                component: Surveys
+            },
+        ]
+
     },
+    
     {
         path: '/login',
         name: 'Login',
@@ -27,5 +46,13 @@ const router = createRouter({
     routes
 });
 
+
+router.beforeEach((to,from,next) => {
+    if(to.meta.requiresAuth && !store.state.user.token ){
+        next({name: 'Login'});
+    }else{
+        next();
+    }
+});
 
 export default router;
