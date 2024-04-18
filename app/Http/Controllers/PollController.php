182b509de0d1f3750284a974a51ae679184dbc86
evansmwenda <evansmwenda.em@gmail.com>
2024-04-19 00:34:31 +0300

    added store answer logic

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePollAnswerRequest;
 use App\Http\Requests\StorePollRequest;
 use App\Http\Requests\UpdatePollRequest;
 use App\Http\Resources\PollResource;
 use App\Models\Poll;
+use App\Models\PollAnswer;
 use App\Models\PollQuestion;
+use App\Models\PollQuestionAnswer;
 use Exception;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
 class PollController extends Controller
 {
@@ -22,7 +27,7 @@ class PollController extends Controller
     public function index(Request $request)
     {
         $user = Auth::user();
-        return PollResource::collection(Poll::where('user_id',$user->id)->paginate());
+        return PollResource::collection(Poll::where('user_id',$user->id)->orderBy('created_at', 'DESC')->paginate(10));
     }
 
     /**
@@ -50,39 +55,6 @@ class PollController extends Controller
         return new PollResource($poll);
     }
 
-    public function saveImage($image){
-        //check if image is valid base64 string
-        if(preg_match('/^data:image\/(\w+);base64,/',$image,$type)){
-            //matches
-            $image = substr($image, strpos($image,',') + 1);
-            $type = strtolower($type[1]);//jpg ,png,gif
-            if(!in_array($type,['jpg','png','gif','jpeg'])){
-                throw new Exception("Did not match image extension");
-            }
-
-            $image = str_replace(' ','+',$image);
-            $image = base64_decode($image);
-
-            if($image == false){
-                throw new \Exception("base64_decode failed");
-            }
-
-            $dir = 'images/';
-            $file = Str::random().'.'.$type;
-            $absolutePath = public_path($dir);
-            $relativePath = $dir.$file;
-            if (!File::exists($absolutePath)){
-                File::makeDirectory($absolutePath,0755,true);
-            }
-
-            file_put_contents($relativePath,$image);
-
-            return $relativePath;
-        }else{
-            throw new Exception("Did not match data URI with image data");
-        }
-    }
-
     /**
      * Display the specified resource.
      */
@@ -92,6 +64,21 @@ class PollController extends Controller
         if($user->id !== $poll->user_id){
             //not the owner of the poll
             abort(403, 'Unauthorized action');
+        }
+
+        return new PollResource($poll);
+    }
+
+    public function showForGuest(Poll $poll)
+    {
+        if (!$poll->status) {
+            return response("", 404);
+        }
+
+        $currentDate = new \DateTime();
+        $expireDate = new \DateTime($poll->expiry_date);
+        if ($currentDate > $expireDate) {
+            return response("", 404);
         }
 
         return new PollResource($poll);
@@ -117,6 +104,34 @@ class PollController extends Controller
         }
 
         $poll->update($data);
+
+        // Get ids as plain array of existing questions
+        $existingIds = $poll->questions()->pluck('id')->toArray();
+        // Get ids as plain array of new questions
+        $newIds = Arr::pluck($data['questions'], 'id');
+        // Find questions to delete
+        $toDelete = array_diff($existingIds, $newIds);
+        //Find questions to add
+        $toAdd = array_diff($newIds, $existingIds);
+
+        // Delete questions by $toDelete array
+        PollQuestion::destroy($toDelete);
+
+        // Create new questions
+        foreach ($data['questions'] as $question) {
+            if (in_array($question['id'], $toAdd)) {
+                $question['poll_id'] = $poll->id;
+                $this->createQuestion($question);
+            }
+        }
+
+        // Update existing questions
+        $questionMap = collect($data['questions'])->keyBy('id');
+        foreach ($poll->questions as $question) {
+            if (isset($questionMap[$question->id])) {
+                $this->updateQuestion($question, $questionMap[$question->id]);
+            }
+        }
         return new PollResource($poll);
     }
 
@@ -141,6 +156,37 @@ class PollController extends Controller
         return response('',204);
     }
 
+
+    public function storeAnswer(StorePollAnswerRequest $request, Poll $poll)
+    {
+        $validated = $request->validated();
+//        var_dump($validated, $survey);
+
+        $surveyAnswer = PollAnswer::create([
+            'poll_id' => $poll->id,
+            'start_date' => date('Y-m-d H:i:s'),
+            'end_date' => date('Y-m-d H:i:s'),
+        ]);
+
+        foreach ($validated['answers'] as $questionId => $answer) {
+            $question = PollQuestion::where(['id' => $questionId, 'survey_id' => $poll->id])->get();
+            if (!$question) {
+                return response("Invalid question ID: \"$questionId\"", 400);
+            }
+
+            $data = [
+                'survey_question_id' => $questionId,
+                'survey_answer_id' => $surveyAnswer->id,
+                'answer' => is_array($answer) ? json_encode($answer) : $answer
+            ];
+
+            $questionAnswer = PollQuestionAnswer::create($data);
+        }
+
+        return response("", 201);
+
+    }
+
     private function createQuestion($data){
         if(is_array($data['data'])){
             $data['data'] = json_encode($data['data']);
@@ -148,7 +194,13 @@ class PollController extends Controller
 
         $validator = Validator::make($data,[
             'question' => 'required|string',
-            'type' => 'required',
+            'type' => ['required', Rule::in([
+                Poll::TYPE_TEXT,
+                Poll::TYPE_TEXTAREA,
+                Poll::TYPE_SELECT,
+                Poll::TYPE_RADIO,
+                Poll::TYPE_CHECKBOX,
+            ])],
             'description' => 'nullable|string',
             'data' => 'present',
             'poll_id' => 'exists:App\Models\Poll,id'
@@ -157,4 +209,69 @@ class PollController extends Controller
         return PollQuestion::create($validator->validated());
 
     }
+
+    private function updateQuestion(PollQuestion $question, $data)
+    {
+        if (is_array($data['data'])) {
+            $data['data'] = json_encode($data['data']);
+        }
+        $validator = Validator::make($data, [
+            'id' => 'exists:App\Models\PollQuestion,id',
+            'question' => 'required|string',
+            'type' => ['required', Rule::in([
+                Poll::TYPE_TEXT,
+                Poll::TYPE_TEXTAREA,
+                Poll::TYPE_SELECT,
+                Poll::TYPE_RADIO,
+                Poll::TYPE_CHECKBOX,
+            ])],
+            'description' => 'nullable|string',
+            'data' => 'present',
+        ]);
+
+        return $question->update($validator->validated());
+    }
+
+    public function saveImage($image){
+        // Check if image is valid base64 string
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            // Take out the base64 encoded text without mime type
+            $image = substr($image, strpos($image, ',') + 1);
+            // Get file extension
+            $type = strtolower($type[1]); // jpg, png, gif
+
+            // Check if file is an image
+            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])) {
+                throw new \Exception('invalid image type');
+            }
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image === false) {
+                throw new \Exception('base64_decode failed');
+            }
+        } else {
+            throw new \Exception('did not match data URI with image data');
+        }
+
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+        if (!File::exists($absolutePath)) {
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+        file_put_contents($relativePath, $image);
+
+        return $relativePath;
+    }
+
+    public function createQuestionAnswer($data)
+    {
+        if (is_array($data['answer'])) {
+            $data['answer'] = json_encode($data['answer']);
+        }
+    }
+
+    
 }
